9f2caa51f84d4263d40ccc05eb40cba3556e1f03
jevdjauon <aleksandar@uon.rs>
2019-04-20 18:05:17 +0200

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,19 +4,49 @@ import { twitter } from "../../icons"
 import Logo from "../../elements/Logo"
 import Text from "../../elements/Text"
 import Container from "../../elements/Container"
+import List from "../../elements/List"
+import styled from "styled-components"
+
+const FooterText = styled.div`
+  max-width: calc(100% / 3);
+`
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  justify-content: space-between;
+`
+
+const items = [
+  { text: "About us", to: "/about-us" },
+  { text: "Nesto", to: "/nesto" },
+  { text: "Drugo", to: "/drugo" },
+]
+
+const iconItems = [
+  { text: "About us", to: "/about-us", icon: twitter },
+  { text: "Nesto", to: "/nesto", icon: twitter },
+  { text: "Drugo", to: "/drugo", icon: twitter },
+]
 
 const Footer = () => {
   return (
     <Container>
-      <h1>Footer</h1>
-      <Icon src={twitter} size={1} text="Lorem ipsum" />
-      <Logo />
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, fugiat.
-        Earum iure facilis nobis ducimus dolore voluptates. Cumque odio quod
-        nulla corporis molestiae. Distinctio assumenda recusandae accusantium
-        quae, dolorum aliquam!
-      </Text>
+      <FooterWrapper>
+        <FooterText>
+          <Logo />
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+            fugiat. Earum iure facilis nobis ducimus dolore voluptates. Cumque
+            odio quod nulla corporis molestiae. Distinctio assumenda recusandae
+            accusantium quae, dolorum aliquam!
+          </Text>
+        </FooterText>
+
+        <List title="Company" items={items} />
+        <List title="Resources" items={items} />
+        <List title="Support" items={items} />
+        <List title="Social" items={iconItems} />
+      </FooterWrapper>
     </Container>
   )
 }
--- src/elements/List.jsx
+++ src/elements/List.jsx
@@ -1,1 +1,59 @@
-import { icons } from "../icons"
+import React from "react"
+import { typography } from "../theme.config"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import Icon from "../elements/Icon"
+
+const ListContainer = styled.div`
+  box-sizing: border-box;
+  padding: 0 2em;
+`
+
+const ListWrapper = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`
+
+const ListItem = styled.li`
+  padding: 0.25em 0;
+  a {
+    ${typography.paragraph};
+    text-decoration: none;
+    color: inherit;
+    opacity: 0.85;
+    transition: opacity 0.3s ease;
+  }
+
+  a:hover {
+    opacity: 1;
+  }
+`
+
+const ListTitle = styled.strong`
+  margin-bottom: 1em;
+  text-transform: uppercase;
+  font-size: 0.9em;
+`
+
+const List = props => {
+  return (
+    <ListContainer>
+      <ListTitle>{props.title}</ListTitle>
+      <ListWrapper>
+        {props.items.map(item => (
+          <ListItem key={item.to}>
+            <Link to={item.to}>
+              {item.icon ? (
+                <Icon src={item.icon} text={item.text} size={1} />
+              ) : (
+                item.text
+              )}
+            </Link>
+          </ListItem>
+        ))}
+      </ListWrapper>
+    </ListContainer>
+  )
+}
+
+export default List
