fd45005abfdb81c1de2a753c6dc9788f09fd9bd2
user <user@tana.uon>
2019-04-20 14:48:26 +0200

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div>
+      <div>nav</div>
+      <div>Logo</div>
+      <div>icons</div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Header from "../components/Header"
 
 const IndexPage = ({ data }) => {
   const meta = data.site.siteMetadata
 
   return (
     <>
+      <Header />
       <h1>{meta.title}</h1>
       <p>{meta.description}</p>
       <p>{meta.author}</p>
